a140c7ef56915bd6b591f4c80e133750979a84cf
tanviralam1998 <68493136+tanviralam1998@users.noreply.github.com>
2021-11-09 15:29:17 +0600

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+    
+    
+    <section class="py-3 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <p>&copy; 2021 PHP</p>
+                </div>
+            </div>
+        </div>
+    </section>   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,36 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>PHP Form</title>
+  </head>
+
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">PHP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collaps" data-bs-target="#navrSupportedContent" 
+            aria-expanded="false" aria-label="Toggle nvigation">
+            <span class="navbar-toggle-icon"></span></button>
+
+            <ul class="navbar-nav ms-auto mb-2">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="form.php">Add User</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">All User</a>
+                </li>
+            </ul>       
+        </div>
+    </nav>
